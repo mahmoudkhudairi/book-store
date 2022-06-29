0eb7d17a05d34c4086f574057ac3e1786136315b
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2022-06-29 04:02:13 +0300

    fix cloudinary saving location

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,8 +1,12 @@
 const cloudinary = require('../configs/cloudinary.config');
 
 const uploadImage = async (bookId, imageUrl) => {
+  console.log('WHATI S', process.env.CLOUDINARY_FOLDER_NAME);
   try {
-    const uploadResponse = await cloudinary.uploader.upload(imageUrl, { public_id: bookId });
+    const uploadResponse = await cloudinary.uploader.upload(imageUrl, {
+      public_id: bookId,
+      folder: process.env.CLOUDINARY_FOLDER_NAME,
+    });
     return uploadResponse.secure_url;
   } catch (err) {
     throw new Error(err.message);
@@ -10,7 +14,7 @@ const uploadImage = async (bookId, imageUrl) => {
 };
 const deleteImage = async (bookId) => {
   try {
-    await cloudinary.uploader.destroy(bookId);
+    await cloudinary.uploader.destroy(`${process.env.CLOUDINARY_FOLDER_NAME}/${bookId}`);
   } catch (err) {
     throw new Error(err.message);
   }
