c460e789b7309479f299d2accf1eac59142442e5
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2022-07-11 22:38:02 +0300

    Adds scroll restoration

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </BrowserRouter>,
 );
 
