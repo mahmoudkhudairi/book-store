897cc57e4478140a68af684d6a410299deded0b9
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2022-07-23 04:36:50 +0300

    serve static build folder for production

--- server/index.js
+++ server/index.js
@@ -8,16 +8,21 @@ const UsersRoutes = require('./routes/user');
 const AuthRoutes = require('./routes/auth');
 const errorHandler = require('./middleware/errorHandler');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use('/api/books', BooksRoutes);
 app.use('/api/users', UsersRoutes);
 app.use(AuthRoutes);
 app.use(errorHandler);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 dbConnection
   .then(() => {
     app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
