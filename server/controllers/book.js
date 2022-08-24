81e80d0cda2f042832a54f32058d91927d095f70
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2022-08-24 22:39:34 +0300

    add publicbook by id route

--- server/controllers/book.js
+++ server/controllers/book.js
@@ -2,36 +2,46 @@ const Book = require('../models/book');
 const axios = require('axios');
 const ErrorResponse = require('../utils/errorResponse');
 const getPublicBooks = async (req, res, next) => {
-  if (process.env.NODE_ENV === 'production') {
-    try {
-      const {
-        data: { items },
-      } = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes?q=programming&maxResults=20&key=${process.env.GOOGLE_BOOKS_API_KEY}`,
-      );
-      const books = items.reduce((acc, item) => {
-        const { volumeInfo, id } = item;
-        const book = {
-          title: volumeInfo.title,
-          authors: volumeInfo.authors,
-          publisher: volumeInfo.publisher,
-          publishedDate: volumeInfo.publishedDate,
-          imageUrl: volumeInfo.imageLinks.thumbnail,
-          description: volumeInfo.description,
-          _id: id,
-        };
+  const booksPerPage = 10;
+  const page = req.query.page === '0' ? 0 : Number(req.query.page) + booksPerPage;
+  console.log('WHAT IS THE QUERY', req.query.page, 'WHAT IS THE PAGE', page);
+  // if (process.env.NODE_ENV === 'production') {
+  try {
+    const {
+      data: { items },
+    } = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=programming&printType=books&orderBy=newest&startIndex=${page}&maxResults=${booksPerPage}&key=${process.env.GOOGLE_BOOKS_API_KEY}`,
+    );
+    let books = [];
+    if (items) {
+      books = items.reduce((acc, item) => {
+        const book = new PublicBook(item);
         acc.push(book);
         return acc;
       }, []);
-
-      res.json(books);
-    } catch (err) {
-      next(new ErrorResponse(err.message));
     }
-  } else {
-    res.json(require('../models/booksApiSample'));
+
+    res.json(books);
+  } catch (err) {
+    next(new ErrorResponse(err.message));
+  }
+  // } else {
+  //   res.json(require('../models/booksApiSample'));
+  // }
+};
+const getPublicBookById = async (req, res, next) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const { data } = await axios.get(`https://www.googleapis.com/books/v1/volumes/${id}`);
+    const book = new PublicBook(data);
+    res.json(book);
+  } catch (err) {
+    next(new ErrorResponse(err.message));
   }
 };
+
 const getBooks = async (req, res, next) => {
   const page = Number(req.query.page) || 0;
   const booksPerPage = 10;
@@ -117,9 +127,23 @@ const getDashboardData = async (req, res, next) => {
 module.exports = {
   getBooks,
   getPublicBooks,
+  getPublicBookById,
   createBook,
   getBookById,
   deleteBook,
   updateBook,
   getDashboardData,
 };
+
+class PublicBook {
+  constructor({ volumeInfo, id }) {
+    const { title, authors, publisher, publishedDate, imageLinks, description } = volumeInfo;
+    this.title = title || 'N/A';
+    this.authors = authors || 'N/A';
+    this.publisher = publisher || 'N/A';
+    this.publishedDate = publishedDate || 'N/A';
+    this.imageUrl = imageLinks ? imageLinks.thumbnail : volumeInfo.previewLink;
+    this.description = description || 'N/A';
+    this._id = id;
+  }
+}
